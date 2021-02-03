cb496c45dcef85b7cf74ba9b37eb5510ea35b015
Gareth Deeney <59611156+GarethDeeney@users.noreply.github.com>
2021-02-03 11:05:37 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -7,17 +7,36 @@ function guessGame() {
 
   var result = document.querySelector(".result");
   var button = document.querySelector(".button");
+  var heading2 = document.querySelector("h2");
   var finger = Math.floor(Math.random() * 6);
+  console.log(finger); //- checks random number
+  var count = 5;
 
+  // Click event listener for button
   button.addEventListener("click", function() {
 
+    // Stores user guess
     var guess = document.querySelector(".guess").value;
 
-    if (guess == finger) {
-      result.innerHTML = "Well done you guessed it!";
+    //Checks guess against random number, if wrong adds 1 to amount of guesses.
+    if (count > 0){
+
+     if(guess > finger) {
+      count--;
+      heading2.innerHTML = "You have " + count + " guesses left.";
+      //console.log(count);
+      result.innerHTML = "Too High! Try Again";
+    } else if (guess < finger){
+      count--;
+      heading2.innerHTML = "You have " + count + " guesses left.";
+      //console.log(count);
+      result.innerHTML = "Too low! Try Again";
     } else {
-      result.innerHTML = "Sorry, the right answer was " + finger;
+      result.innerHTML = "Well Done! The correct answer was " + finger + ". It took you " + count + " guesses.";
     }
-    
+  } else {
+    heading2.innerHTML = "Sorry You have no guesses left.";
+  }
+
   });
 }
